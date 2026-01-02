73d313cab3e7258ed5d3a35a1304afa9b64b7f12
Dylan Lin <dylan.lin@jkopay.com>
2026-01-02 14:38:57 +0800

    chore: upgrade Node.js to v20 and update contentlayer dependencies

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -44,7 +44,7 @@ export const Blog = defineDocumentType(() => ({
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
-    categories: { type: 'string'},
+    categories: { type: 'string' },
     tags: { type: 'list', of: { type: 'string' } },
     lastmod: { type: 'date' },
     draft: { type: 'boolean' },
@@ -65,7 +65,7 @@ export const Coding = defineDocumentType(() => ({
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
-    categories: { type: 'string'},
+    categories: { type: 'string' },
     tags: { type: 'list', of: { type: 'string' } },
     lastmod: { type: 'date' },
     draft: { type: 'boolean' },
